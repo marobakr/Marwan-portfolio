eeeff80bdb5c59a14cd9598da068006687903e06
marwanabubakr <dev@marobakr>
2024-04-08 22:42:10 +0200

    test animations after viewinit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,11 @@ import { AfterViewInit, Component } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   constructor() {}
   isLoding: boolean = true;
+  afterViewInit: boolean = false;
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.isLoding = false;
     }, 1000);
+    this.afterViewInit = true;
   }
 }
