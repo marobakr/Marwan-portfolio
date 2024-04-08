f6c79d1e842ba4a165251bb6c7177eb6ced64f24
marwanabubakr <dev@marobakr>
2024-04-08 21:24:52 +0200

    handle cv view

--- src/app/core/pages/home/components/canvas/particles.ts
+++ src/app/core/pages/home/components/canvas/particles.ts
@@ -1,10 +1,9 @@
 export const ParticlesConfig = {
   particles: {
     number: {
-      value: 200,
+      value: 300,
       density: {
         enable: false,
-        value_area: 800,
       },
     },
     color: {
@@ -44,8 +43,6 @@ export const ParticlesConfig = {
       bounce: false,
       attract: {
         enable: false,
-        rotateX: 600,
-        rotateY: 1200,
       },
     },
   },
