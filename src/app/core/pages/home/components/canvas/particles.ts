2153100768b71f5b09d932c7b42d26d177d4e4a1
marwanabubakr <dev@marobakr>
2024-04-09 19:12:48 +0200

    add another portfolio

--- src/app/core/pages/home/components/canvas/particles.ts
+++ src/app/core/pages/home/components/canvas/particles.ts
@@ -1,97 +1,6 @@
 export const ParticlesConfig = {
   particles: {
     number: {
-      value: 200,
-      density: {
-        enable: false,
-      },
-    },
-    color: {
-      value: '#ffffff',
-    },
-
-    opacity: {
-      value: 1,
-      random: false,
-      anim: {
-        enable: true,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
-    },
-    size: {
-      value: 2,
-      random: true,
-      anim: {
-        enable: true,
-        speed: 10,
-        size_min: 0.1,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: false,
-    },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: 'none',
-      random: false,
-      straight: false,
-      out_mode: 'out',
-      bounce: false,
-      attract: {
-        enable: false,
-      },
-    },
-  },
-  interactivity: {
-    detect_on: 'canvas',
-    events: {
-      onhover: {
-        enable: false,
-        mode: 'repulse',
-      },
-      onclick: {
-        enable: false,
-        mode: 'push',
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 8,
-        speed: 3,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
-    },
-  },
-  retina_detect: true,
-};
-
-/*
-export const ParticlesConfig = {
-  particles: {
-    number: {
       value: 160,
       density: {
         enable: true,
@@ -132,5 +41,3 @@ export const ParticlesConfig = {
   },
   retina_detect: true,
 };
-
-*/
