89759364113d53ebd97ac20c0308f4bf35bff14d
marwanabubakr <dev@marobakr>
2024-07-17 01:51:58 +0300

    SEO

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { inject } from '@vercel/analytics';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    providers: [
+      {
+        provide: APP_INITIALIZER,
+        useFactory: () => () => {
+          inject({ mode: isDevMode() ? 'development' : 'production' });
+        },
+        multi: true,
+      },
+    ],
+  })
+  .catch((err) => console.error(err));
